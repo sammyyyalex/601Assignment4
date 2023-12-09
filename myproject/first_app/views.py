456a62518b1df20ff3753a37c854776fc1b14039
Sam <saparadero@gmail.com>
2023-12-09 01:02:33 -0500

    rounds to two decimals

--- myproject/first_app/views.py
+++ myproject/first_app/views.py
@@ -8,18 +8,19 @@ class BillForm(forms.Form):
     tipPercentage = forms.DecimalField(label='Tip Percentage',widget=forms.TextInput(attrs={'placeholder': 'Enter tip percentage'}))
 
 def calculate_tip(request):
+    totalAmount = None
     if request.method == 'POST':
         form = BillForm(request.POST)
         if form.is_valid():
             subtotal = form.cleaned_data['subtotal']
             tipPercentage = form.cleaned_data['tipPercentage']
             tipAmount = (tipPercentage / 100) * subtotal
-            totalAmount = subtotal + tipAmount
-            return render(request, 'index.html', {'totalAmount': totalAmount})
+            totalAmount = round(subtotal + tipAmount, 2)
+
     else:
         form = BillForm()
 
-    return render(request, 'index.html', {'form': form})
+    return render(request, 'index.html', {'form': form, 'totalAmount': totalAmount})
 
 def index(request):
    my_dict = {'insert_me':"Hello I am from views.py!"}
